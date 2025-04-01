cec54040ed0e060d08486c6b59133ca43933ff62
yuki-f-saka <ffffna0lucky+github@gmail.com>
2025-04-02 01:48:39 +0900

    vercelのAnalytics導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <div className="flex-1 flex flex-col items-center ">
           {children}
           <SpeedInsights />
+          <Analytics />
         </div>
       </body>
     </html>
